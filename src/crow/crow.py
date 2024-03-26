26643a740f2aab9d99834b805dcec04dfb702347
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-03-26 12:01:17 +0900

    Feat try catch exception

--- src/crow/crow.py
+++ src/crow/crow.py
@@ -1,9 +1,21 @@
-import time
 import requests
+from logging import getLogger
+
+
+logger = getLogger(__name__)
 
 
 def req():
-    response = requests.get("https://note.lapras.com/")
-    response.raise_for_status()
+    response = requests.get("https://oshi-connect.com/")
+
+    try:
+        response.raise_for_status()
+    except requests.RequestException as e:
+        if e.response is not None:
+            logger.exception("response error", e.response.text)
+        else:
+            logger.exception("response error")
+    else:
+        logger.exception("unexpected error")
 
     print(response.text)
